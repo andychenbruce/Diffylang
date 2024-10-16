b288ad0109bea1503044654dafdab6becb2da354
陈功 <chengong456@qq.com>
2024-10-15 18:39:22 -0700

    type inference skeleton not working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import json
 import subprocess
+import modules.parsetree
+
 
 def main():
     sub_proc = subprocess.run(["cargo", "run", "--quiet"], capture_output=True, text=True)
