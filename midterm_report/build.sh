f58252067924e986c235ac4f7fa748fbd4d562ea
陈功 <chengong456@qq.com>
2024-12-13 00:37:39 -0800

    add stuff to final report

--- midterm_report/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-pdflatex midterm_report
-bibtex midterm_report
-pdflatex midterm_report
-pdflatex midterm_report
--- proposal/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-lualatex proposal
-bibtex proposal
-lualatex proposal
-lualatex proposal
-
