d0790c67bf3cddd0fadbb17d092e7cd6426c72f4
陈功 <chengong456@qq.com>
2024-12-14 03:07:06 +0000

    save progress

--- final_report/analysis/plot.py
+++ final_report/analysis/plot.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import json
 
-data = [0.3795374747659379, 0.32269978092069185, 0.2837195222399832, 0.26560266400647004, 0.25876356426930824]
+
+
+data = json.loads(open("./for_loop.json", "rb").read())
 fig, ax = plt.subplots()
 ax.plot(data)
 ax.set_xlabel('epoch')
 ax.set_ylabel('loss')
-plt.xticks(list(range(len(data))))
+# plt.xticks(list(range(len(data))))
 
 plt.savefig("test.png")
